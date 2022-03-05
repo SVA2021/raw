459127c6ff223bd3bba46e4cfc1ceb8d1ac96fa6
SVA2021 <sofronov.vit@gmail.com>
2022-03-05 22:44:43 +0400

    unused files deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import { Counter } from './features/counter/Counter';
 // import './App.css';
 import './styles/global.scss';
 import Logo from './components/Logo';
