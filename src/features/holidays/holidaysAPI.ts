afbf744ea138feb55c2eb5ade53b347ec4e81db5
SVA2021 <sofronov.vit@gmail.com>
2022-03-09 23:15:36 +0400

    refactoring holiday API functions

--- src/features/holidays/holidaysAPI.ts
+++ src/features/holidays/holidaysAPI.ts
@@ -1,13 +1,9 @@
 import axios from "axios";
 // const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
-// const PROXY_URL = '';
 const BASE_URL = 'https://date.nager.at';
 const COUNTRIES_LIST_URL = '/api/v3/AvailableCountries';
 const NEXT_WEEK_URL = '/api/v3/NextPublicHolidaysWorldwide';
 const LIST_OF_HOLIDAYS_URL = '/api/v3/PublicHolidays';
-
-let today = new Date();
-let year = today.getFullYear();
 
 const holidayInstance = axios.create({
 	// baseURL: PROXY_URL + BASE_URL,
@@ -39,7 +35,11 @@ export const holidaysAPI = {
 	},
 
 	getCountryHolydays(countryCode = 'RU') {
-		return holidayInstance.get(LIST_OF_HOLIDAYS_URL + `/` + year + `/` + countryCode)
+		let today = new Date();
+		let year = today.getFullYear();
+		let query = LIST_OF_HOLIDAYS_URL + `/` + year + `/` + countryCode;
+
+		return holidayInstance.get(query)
 			.then(response => {
 				return response.data;
 			})
