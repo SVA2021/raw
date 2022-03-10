1f4dfdda87c1a55e240ae875289cee10434a8346
SVA2021 <sofronov.vit@gmail.com>
2022-03-10 13:28:56 +0400

    getTodayDateString function added to common

--- src/app/commonFunctions.ts
+++ src/app/commonFunctions.ts
@@ -1,52 +1,61 @@
 import { useEffect, useRef } from 'react';
 
 export function usePrevious(value: any) {
-    const ref = useRef(null);
-    useEffect(() => {
-        ref.current = value;
-    });
-    return ref.current;
+	const ref = useRef(null);
+	useEffect(() => {
+		ref.current = value;
+	});
+	return ref.current;
 }
 
 export function isEmpty(obj: any) {
-    for (const prop in obj) {
-        if (obj.hasOwnProperty(prop))
-            return false;
-    }
-    return true;
+	for (const prop in obj) {
+		if (obj.hasOwnProperty(prop))
+			return false;
+	}
+	return true;
 }
 
 export function getDiff(o1: any, o2: any) {
-    let diff = {};
-    if (isEmpty(o1) || isEmpty(o2)) return diff;
-    diff = Object.keys(o2).reduce((diff, key) => {
-        if (o1[key] === o2[key]) return diff
-        return {
-            ...diff,
-            [key]: o2[key]
-        }
-    }, {});
-    return diff;
+	let diff = {};
+	if (isEmpty(o1) || isEmpty(o2)) return diff;
+	diff = Object.keys(o2).reduce((diff, key) => {
+		if (o1[key] === o2[key]) return diff
+		return {
+			...diff,
+			[key]: o2[key]
+		}
+	}, {});
+	return diff;
 }
 
 export const propertyCopy = (obj: any) => {
-    let result: any = {};
-    for (const key in obj) {
-        if (Object.hasOwnProperty.call(obj, key)) {
-            result[key] = obj[key].name;
-        }
-    }
-    return result;
+	let result: any = {};
+	for (const key in obj) {
+		if (Object.hasOwnProperty.call(obj, key)) {
+			result[key] = obj[key].name;
+		}
+	}
+	return result;
 }
 
-export const setArray = (qty: number, array: Array<any>, item = '') => {
-    array.length = 0;
-    for (let i = 0; i < qty; i++) {
-        array.push(i + item);
-    }
-    return array;
+export const setArray = (qty: number, array: any[], item = '') => {
+	array.length = 0;
+	for (let i = 0; i < qty; i++) {
+		array.push(i + item);
+	}
+	return array;
 }
 
 export const normalizeObjText = (str: any) => {
-    return JSON.stringify(str, null, ' ').replace(/"/gm, '');
+	return JSON.stringify(str, null, ' ').replace(/"/gm, '');
+}
+
+export const getTodayDateString = (divider = '') => {
+	let today = new Date();
+	let day = today.getDate();
+	let month = today.getMonth() + 1;
+	let year = today.getFullYear();
+	let result = String(year + divider + month + divider + day);
+	return result;
 }
