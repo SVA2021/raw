5c8906a2a4d3a5480b6285bea1559fb3b09e4808
SVA2021 <sofronov.vit@gmail.com>
2022-03-11 00:48:43 +0400

    typography components added

--- src/components/common/Typography.tsx
+++ src/components/common/Typography.tsx
@@ -1,51 +1,97 @@
+import classNames from 'classnames';
 import { FC } from 'react';
 import s from './Typography.module.scss';
 
+type TTypographyProps = {
+	className?: string;
+	withOffset?: boolean;
+	style?: any;
+};
+
 export const HeroTitle: FC<any> = ({ children }) => (
-    <h1 className={s.heroTitle}>
-        {children}
-    </h1>
+	<h1 className={s.heroTitle}>
+		{children}
+	</h1>
 );
 
 export const SectionTitle: FC<any> = ({ children }) => (
-    <h3 className={s.sectionTitle}>
-        {children}
-    </h3>
+	<h3 className={s.sectionTitle}>
+		{children}
+	</h3>
 );
 
 export const SubTitle: FC<any> = ({ children }) => (
-    <h4 className={s.subTitle}>
-        {children}
-    </h4>
+	<h4 className={s.subTitle}>
+		{children}
+	</h4>
 );
 
 export const SubTitleUpper: FC<any> = ({ children }) => (
-    <h4 className={s.subTitleUpper}>
-        {children}
-    </h4>
+	<h4 className={s.subTitleUpper}>
+		{children}
+	</h4>
 );
 
 export const SubTitleUpperDark: FC<any> = ({ children }) => (
-    <h4 className={s.subTitleUpper__dark}>
-        {children}
-    </h4>
+	<h4 className={s.subTitleUpper__dark}>
+		{children}
+	</h4>
 );
 
 export const Strong: FC<any> = ({ children }) => (
-    <strong className={s.strong}>
-        {children}
-    </strong>
+	<strong className={s.strong}>
+		{children}
+	</strong>
 );
 
-export const Highlighted: FC<any> = ({ children }) => (
-    <p className={s.highlighted}>
-        {children}
-    </p>
+export const Highlighted: FC<TTypographyProps> = ({ className = '', style, withOffset, children }) => (
+	<span
+		className={classNames(className, s.highlighted, {
+			[s.withOffset]: withOffset,
+		})}
+		style={style}
+	>
+		{children}
+	</span>
 );
-
 
 export const IconLabel: FC<any> = ({ children }) => (
-    <p className={s.icon_label}>
-        {children}
-    </p>
+	<p className={s.icon_label}>
+		{children}
+	</p>
+);
+
+
+export const PlainText: FC<TTypographyProps> = ({ className = '', style, withOffset, children }) => (
+	<p
+		className={classNames(className, s.plainText, {
+			[s.withOffset]: withOffset,
+		})}
+		style={style}
+	>
+		{children}
+	</p>
+);
+
+
+export const Title: FC<TTypographyProps> = ({ className = '', style, withOffset, children }) => (
+	<span
+		className={classNames(className, s.title, {
+			[s.withOffset]: withOffset,
+		})}
+		style={style}
+	>
+		{children}
+	</span>
+);
+
+export const InlineText: FC<TTypographyProps> = ({ className = '', style, withOffset, children }) => (
+	<span
+		className={classNames(className, s.inlineText, {
+			[s.withOffset]: withOffset,
+		})}
+		style={style}
+	>
+		{children}
+	</span>
 );
