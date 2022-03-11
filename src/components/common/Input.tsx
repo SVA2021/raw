fe9cfa793a527003bf5296cb888c34ad1b9567f5
SVA2021 <sofronov.vit@gmail.com>
2022-03-11 15:02:47 +0400

    select added to inputs

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,19 +1,34 @@
 import styles from './Input.module.scss';
 
 export const InputRange = (props: any) => {
-  return (
-    <input type="range" className={styles.slider} {...props} />
-  )
+	return (
+		<input type="range" className={styles.slider} {...props} />
+	)
 };
 
 export const InputNumber = (props: any) => {
-  return (
-    <input type="number" className={styles.number} {...props} />
-  )
+	return (
+		<input type="number" className={styles.number} {...props} />
+	)
 };
 
 export const InputText = (props: any) => {
-  return (
-    <input type="text" className={styles.text} {...props} />
-  )
+	return (
+		<input type="text" className={styles.text} {...props} />
+	)
 };
+
+
+export const Select = (props: any) => {
+	return (
+		<label>
+			{props.label}
+			<select value={props.value} onChange={props.handleChange}>
+				{props.optionList.map(
+					(option: any) => <option value={option.value}>{option.text}</option>
+				)
+				}
+			</select>
+		</label>
+	)
+}
