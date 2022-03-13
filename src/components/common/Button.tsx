9869e32de380bb19f76891bada6f858a7ea1ffec
SVA2021 <sofronov.vit@gmail.com>
2022-03-13 21:58:42 +0400

    button active mode added

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,13 +7,15 @@ type TButtonProps = {
 	className?: string;
 	onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 	withOffset?: boolean;
+	active?: boolean;
 };
 
-const Button: FC<TButtonProps> = ({ className = '', withOffset, children, ...props }) => (
+const Button: FC<TButtonProps> = ({ className = '', withOffset, active, children, ...props }) => (
 	<button
-		className={classNames(className, styles.button, {
+		className={classNames(styles.button, {
 			[styles.withOffset]: withOffset,
-		})}
+			[styles.active]: active,
+		}, className)}
 		{...props}
 	>
 		{children}
