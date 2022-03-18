305691d6edc67f6233976aea02380648b7ed34de
SVA2021 <sofronov.vit@gmail.com>
2022-03-19 00:40:00 +0400

    main concept of weather component finished

--- src/features/weather/Weather.tsx
+++ src/features/weather/Weather.tsx
@@ -1,21 +1,19 @@
 import { SectionTitle } from "../../components/common/Typography";
 import { useAppSelector } from '../../app/hooks';
-import { getTodayDateString } from "../../app/commonFunctions";
 import WeatherSettings from "./WeatherSettings";
 import { selectWeather, selectWeatherQueryStatus } from "./weatherSlice";
 import WeatherItem from "./WeatherItem";
 import Loading from "../../components/Loading";
 
-const Weather = () => {
+const Weather = (props: any) => {
 
-	const today = getTodayDateString('-');
 	const queryStatus = useAppSelector(selectWeatherQueryStatus);
 	const weather = useAppSelector(selectWeather);
 
 	return (
 		<div className="weather">
 			<SectionTitle>Weather</SectionTitle>
-			<WeatherSettings today={today} />
+			<WeatherSettings />
 			{
 				(queryStatus === 'loading' || queryStatus === 'failed')
 					? <Loading text={queryStatus} />
