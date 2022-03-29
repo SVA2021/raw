1ca847e31d6d2a9a787333bdb929ce79b4b134fc
SVA2021 <sofronov.vit@gmail.com>
2022-03-29 16:34:54 +0400

    pomodoro settings created

--- src/features/pomodoro/PomodoroSettings.tsx
+++ src/features/pomodoro/PomodoroSettings.tsx
@@ -1,37 +1,66 @@
-import { InlineText, PlainText, SectionTitle, Strong, SubTitle, Title } from "../../components/common/Typography";
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { SubTitle, } from "../../components/common/Typography";
+import { useAppDispatch } from '../../app/hooks';
 import Button from "../../components/common/Button";
 import s from './Pomodoro.module.scss';
-import { InputRange } from "../../components/common/Input";
+import { DEFAULT_TIMERS, setSettingsStatus, setSettingsValues } from "./pomodoroSlice";
+import { useState } from "react";
+import { InputRangeModule } from "../../components/common/InputRangeModule/InputRangeModule";
 
 const PomodoroSettings = () => {
+	const dispatch = useAppDispatch();
+
+	const setDefaultTimers = () => {
+		dispatch(setSettingsValues(DEFAULT_TIMERS));
+	}
+
+	const closeSettings = () => {
+		dispatch(setSettingsStatus(false));
+	}
+
+	const workLimit = { min: 1, max: 30 };
+	const [workTimer, setWorkTimer] = useState(DEFAULT_TIMERS.work);
+
+	const shortBreakLimit = { min: 1, max: 10 };
+	const [shortBreakTimer, setShortBreakTimer] = useState(DEFAULT_TIMERS.short);
+
+	const longBreakLimit = { min: 1, max: 30 };
+	const [longBreakTimer, setLongBreakTimer] = useState(DEFAULT_TIMERS.long);
+
+	const setTimers = () => {
+		dispatch(setSettingsValues(
+			{
+				work: workTimer,
+				short: shortBreakTimer,
+				long: longBreakTimer
+			}
+		));
+		dispatch(setSettingsStatus(false));
+	}
+
 	return (
 		<div className={s.settings}>
 			<SubTitle>settings</SubTitle>
-			<fieldset className={s.settings__item}>
-				<legend>work</legend>
-				<InlineText withOffset={true}>0</InlineText>
-				<InputRange min="0" max="20" value={0} name="work"
-					onChange={(e: React.ChangeEvent<HTMLInputElement>) => alert(+(e.target.value))} />
-				<InlineText withOffset={true}>25</InlineText>
-			</fieldset>
-			<fieldset className={s.settings__item}>
-				<legend>short break</legend>
-				<InlineText withOffset={true}>0</InlineText>
-				<InputRange min="0" max="20" value={0} name="short break"
-					onChange={(e: React.ChangeEvent<HTMLInputElement>) => alert(+(e.target.value))} />
-				<InlineText withOffset={true}>25</InlineText>
-			</fieldset>
-			<fieldset className={s.settings__item}>
-				<legend>long break</legend>
-				<InlineText withOffset={true}>0</InlineText>
-				<InputRange min="0" max="20" value={0} name="long break"
-					onChange={(e: React.ChangeEvent<HTMLInputElement>) => alert(+(e.target.value))} />
-				<InlineText withOffset={true}>25</InlineText>
-			</fieldset>
-			<Button>set timers</Button>
-			<Button>reset</Button>
-			<button className={s.closeBtn} onClick={() => alert('close')}>X</button>
+			<InputRangeModule
+				className={s.settings__item}
+				description={'work'}
+				value={workTimer} min={workLimit.min} max={workLimit.max}
+				onChangeFunction={(val: number) => setWorkTimer(val)}
+			/>
+			<InputRangeModule
+				className={s.settings__item}
+				description={'short'}
+				value={shortBreakTimer} min={shortBreakLimit.min} max={shortBreakLimit.max}
+				onChangeFunction={(val: number) => setShortBreakTimer(val)}
+			/>
+			<InputRangeModule
+				className={s.settings__item}
+				description={'long'}
+				value={longBreakTimer} min={longBreakLimit.min} max={longBreakLimit.max}
+				onChangeFunction={(val: number) => setLongBreakTimer(val)}
+			/>
+			<Button onClick={() => setTimers()}>set timers</Button>
+			<Button onClick={() => setDefaultTimers()}>reset</Button>
+			<button className={s.closeBtn} onClick={() => closeSettings()}>X</button>
 		</div>
 	)
 }
