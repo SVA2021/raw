483fd7e04565331c1427ba31459f6eb3a0cd555e
SVA2021 <sofronov.vit@gmail.com>
2022-03-31 01:11:18 +0400

    clock comments deleted

--- src/features/pomodoro/PomodoroClock.tsx
+++ src/features/pomodoro/PomodoroClock.tsx
@@ -1,33 +1,23 @@
 import { useEffect, useState } from "react";
 import { normalizeDateString } from "../../app/commonFunctions";
-// import { useAppSelector } from "../../app/hooks";
 import { InlineText, } from "../../components/common/Typography";
 import s from './Pomodoro.module.scss';
 import { chooseColor } from "./pomodoroFunctions";
-// import { selectSettings } from "./pomodoroSlice";
 
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
 
 const PomodoroClock = (props: any) => {
-	// const timers = useAppSelector(selectSettings);
-	const time = props.time;
+
+	const time = props.time * MINUTE;
 	const mode = props.mode;
 	const isRunning = props.isRunning;
-	// const workTime = props.timers.work * MINUTE;
-	// const workTime = timers.work * MINUTE;
-	// const longBreakTime = props.long * MINUTE;
-	// const shortBreakTime = props.short * MINUTE;
 
 	const [clockColor, setClockColor] = useState('');
 	useEffect(() => setClockColor(chooseColor(mode, isRunning)), [mode, isRunning]);
 
-	// const [time, setTime] = useState(0);
 	const timeMin = Math.floor(time / MINUTE);
 	const timeSec = normalizeDateString((time - (timeMin * MINUTE)) / SECOND);
-
-	// useEffect(() => setTime(longBreakTime),[longBreakTime]);
-	// useEffect(() => setTime(workTime),[workTime]);
 
 	return (
 		<div className={s.clock} style={{ backgroundColor: clockColor }}>
