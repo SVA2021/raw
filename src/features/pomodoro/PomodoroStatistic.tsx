b80109a719b9544525d8acacf4a07e3a70029eb7
SVA2021 <sofronov.vit@gmail.com>
2022-04-01 20:10:02 +0400

    statistic pomodoro created

--- src/features/pomodoro/PomodoroStatistic.tsx
+++ src/features/pomodoro/PomodoroStatistic.tsx
@@ -1,29 +1,44 @@
-import { InlineText, PlainText, Title } from "../../components/common/Typography";
+import { Highlighted,  PlainText, Title } from "../../components/common/Typography";
 import { useAppSelector } from '../../app/hooks';
 import s from './Pomodoro.module.scss';
 import { selectMode, selectStatistic } from "./pomodoroSlice";
-import { nextModePomodoro } from "./pomodoroFunctions";
+import { nextModePomodoro, normalizeSession } from "./pomodoroFunctions";
+import { convertMinuteToHHMM } from "../../app/commonFunctions";
 
 const PomodoroStatistic = () => {
 	const mode = useAppSelector(selectMode);
 	const statistic = useAppSelector(selectStatistic);
 	const nextMode = nextModePomodoro(mode, statistic);
-
+	const session = normalizeSession(statistic.sessions);
+	const workStat = convertMinuteToHHMM(statistic.work);
+	const relaxStat = convertMinuteToHHMM(statistic.break);
 	return (
-		<div className={s.statistic}>
-			<PlainText>
-				<Title>status:</Title>
-				<InlineText withOffset={true}>{mode}</InlineText>
-			</PlainText>
-			<PlainText>
-				<Title>next:</Title>
-				<InlineText withOffset={true}>{nextMode}</InlineText>
-			</PlainText>
-			<PlainText>
-				<Title>session:</Title>
-				<InlineText withOffset={true}>{statistic.sessions} / 4</InlineText>
-			</PlainText>
-		</div>
+		<>
+			<div className={s.statistic}>
+				<PlainText>
+					<Title>status:</Title>
+					<Highlighted withOffset={true}>{mode}</Highlighted>
+				</PlainText>
+				<PlainText>
+					<Title>next:</Title>
+					<Highlighted withOffset={true}>{nextMode}</Highlighted>
+				</PlainText>
+				<PlainText>
+					<Title>session:</Title>
+					<Highlighted withOffset={true}>{session} / 4</Highlighted>
+				</PlainText>
+			</div>
+			<div className={s.statistic}>
+				<PlainText>
+					<Title>total work time:</Title>
+					<Highlighted withOffset={true}>{workStat}</Highlighted>
+				</PlainText>
+				<PlainText>
+					<Title>total relax time:</Title>
+					<Highlighted withOffset={true}>{relaxStat}</Highlighted>
+				</PlainText>
+			</div>
+		</>
 	)
 }
 
