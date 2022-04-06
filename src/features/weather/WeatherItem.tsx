6624b112ce0603a4a0b426118faf81e45d621e98
SVA2021 <sofronov.vit@gmail.com>
2022-04-06 12:04:46 +0400

    weather style and structure correction for desktop

--- src/features/weather/WeatherItem.tsx
+++ src/features/weather/WeatherItem.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { getWeatherIconURL, setColor } from './weatherFunctions';
 import Loading from '../../components/common/Loading/Loading';
 import { getDateString } from '../../app/commonFunctions';
+import classNames from 'classnames';
 
 const WeatherItem = (props: any) => {
 	const weather: WeatherType = props.weather;
@@ -23,39 +24,54 @@ const WeatherItem = (props: any) => {
 	return (
 		<div className={s.main}>
 			<PlainText>
-				<Title>date:</Title>
-				<InlineText withOffset={true}>{date}</InlineText>
-				<Title>city:</Title>
-				<InlineText withOffset={true}>{weather.name}</InlineText>
-			</PlainText>
-			<div className={s.icon} style={{ backgroundImage: `url(${iconURL})` }}></div>
-			<PlainText className={s.description}>
-				{weather.description}
+				<Title withOffset={true}>date:</Title>
+				{date}
 			</PlainText>
 			<PlainText>
-				<Title>temperature:</Title>
-				<InlineText style={setColor(weather.temp)} withOffset={true}>{weather.temp}&#176;C</InlineText>
+				<Title withOffset={true}>city:</Title>
+				{weather.name}
+			</PlainText>
+			<div className={s.description}>
+				<div className={s.icon} style={{ backgroundImage: `url(${iconURL})` }}></div>
+				<PlainText >
+					<InlineText className={classNames(s.temperature, s[setColor(weather.temp)])} >{weather.temp}&#176;C</InlineText>
+					<InlineText className={s.parameter}>{weather.description}</InlineText>
+				</PlainText>
+			</div>
+			<PlainText>
 				<Title>feels like:</Title>
-				<InlineText style={setColor(weather.feels_like)} withOffset={true}>{weather.feels_like}&#176;C</InlineText>
+				<InlineText className={s[setColor(weather.feels_like)]} withOffset={true}>
+					{weather.feels_like}&#176;C
+				</InlineText>
+			</PlainText>
+			<PlainText>
 				<Title>min:</Title>
-				<InlineText style={setColor(weather.temp_min)} withOffset={true}>{weather.temp_min}&#176;C</InlineText>
+				<InlineText className={s[setColor(weather.temp_min)]} withOffset={true}>
+					{weather.temp_min}&#176;C
+				</InlineText>
 				<Title>max:</Title>
-				<InlineText style={setColor(weather.temp_max)} withOffset={true}>{weather.temp_max}&#176;C</InlineText>
+				<InlineText className={s[setColor(weather.temp_max)]} withOffset={true}>
+					{weather.temp_max}&#176;C
+				</InlineText>
+			</PlainText>
+			<PlainText>
+				<Title>wind:</Title>
+				<InlineText withOffset={true}>{weather.windSpeed}m/s</InlineText>
+				<InlineText className={s.wind} style={{ transform: `rotate(${weather.windDeg}deg)` }}>
+					&#8593;
+				</InlineText>
+			</PlainText>
+			<PlainText>
+				<Title>humidity:</Title>
+				<InlineText withOffset={true}>{weather.humidity}%</InlineText>
 			</PlainText>
 			<PlainText>
 				<Title>pressure:</Title>
 				<InlineText withOffset={true}>{weather.pressure}hPa</InlineText>
-				<Title>humidity:</Title>
-				<InlineText withOffset={true}>{weather.humidity}%</InlineText>
-				<Title>visibility:</Title>
-				<InlineText withOffset={true}>{weather.visibility}meter</InlineText>
 			</PlainText>
 			<PlainText>
-				<Title>wind:</Title>
-				<InlineText withOffset={true}>{weather.windSpeed}meter/sec</InlineText>
-				<InlineText className={s.wind} style={{ transform: `rotate(${weather.windDeg}deg)` }}>
-					&#8593;
-				</InlineText>
+				<Title>visibility:</Title>
+				<InlineText withOffset={true}>{weather.visibility}m</InlineText>
 			</PlainText>
 		</div>
 	)
