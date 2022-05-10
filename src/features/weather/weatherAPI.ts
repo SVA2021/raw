f755417a1b448ca2dd76b721674449d14a2636b3
SVA2021 <sofronov.vit@gmail.com>
2022-05-10 21:02:02 +0400

    fix: correct api key to open

--- src/features/weather/weatherAPI.ts
+++ src/features/weather/weatherAPI.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { API_KEY_WEATHER } from "../../private/weatherVariables";
 import { CityGeoType } from "./weatherTypes";
 
-export const BASE_URL = 'http://api.openweathermap.org';
+export const BASE_URL = 'https://api.openweathermap.org';
 const CURRENT_WEATHER = '/data/2.5/weather';
 const GEO_CITY = '/geo/1.0/direct?q=';
+const API_KEY_OPEN_WEATHER = '&appid=a7eb9522e11fdcf356f7ca73fdf3a8a2';
 
 const weatherInstance = axios.create({
 	baseURL: BASE_URL,
@@ -13,8 +13,8 @@ const weatherInstance = axios.create({
 export const weatherAPI = {
 
 	getCurrentWeather(geo: CityGeoType) {
-		let query = CURRENT_WEATHER + `?lat=${geo.lat}&lon=${geo.lon}` + API_KEY_WEATHER + `&units=metric`;
-		return weatherInstance.get(query)
+		let query = CURRENT_WEATHER + `?lat=${geo.lat}&lon=${geo.lon}` + API_KEY_OPEN_WEATHER + `&units=metric`;
+		return weatherInstance.post(query)
 			.then(response => {
 				console.log(response);
 				return response;
@@ -27,7 +27,7 @@ export const weatherAPI = {
 
 	// search on different countries will be added later 
 	getCityGeo(city = 'Moscow', countryCode = 'ru', limit = 5) {
-		const query = GEO_CITY + `${city},${countryCode}&limit=${limit}` + API_KEY_WEATHER;
+		const query = GEO_CITY + `${city},${countryCode}&limit=${limit}` + API_KEY_OPEN_WEATHER;
 		return weatherInstance.get(query)
 			.then(response => {
 				return response;
