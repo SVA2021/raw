3c4acb4cce9ac0030a2b59aff4c5c0f128847b5d
SVA2021 <sofronov.vit@gmail.com>
2022-05-18 16:29:01 +0400

    upd: accessibility improvement

--- src/components/LinkToOriginal.tsx
+++ src/components/LinkToOriginal.tsx
@@ -3,13 +3,31 @@ import { ReactComponent as Icon } from '../img/earth_globe.svg';
 const LinkToOriginal = (props: any) => {
 	const link = props.link;
 	const height = props.height;
+
+	const linkStyle: React.CSSProperties = {
+		position: 'relative', 
+		padding: '4px',
+		borderRadius: '8px'
+	}
+	
+	const srOnly: React.CSSProperties = {
+		height: '0px',
+		width: '0px',
+		opacity: '0',
+		display: 'block',
+		position: "absolute",
+		left: '10000px'
+	}
+
 	return (
 		<a
 			href={link}
 			target="_blank"
 			rel="noopener noreferrer"
+			style={linkStyle}
 		>
 			<Icon style={{ height: height }} />
+			<span style={srOnly}>link to original service provider</span>
 		</a>
 	)
 }
